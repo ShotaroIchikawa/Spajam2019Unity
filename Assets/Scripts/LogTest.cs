4e726f90a315dfec077b9cbe8e8ef225d12ca301
Shotaro <bd.th.8219@gmail.com>
2019-06-17 02:13:28 +0900

    Add Camera, Weater Components
    どちらもサンプルコードを入れたが，うまく動いているのか微妙
    CameraはBuildすると動くが，WeatherはBuildしない時の方が動く

---
+++ Assets/Scripts/LogTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogTest : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            Debug.LogFormat("Time:{0}", System.DateTime.Now);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+} // class TestComponent
