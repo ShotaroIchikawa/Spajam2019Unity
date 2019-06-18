71c6b9250e50ab813af141f3ff813e7602ed17ba
Shotaro <33152696+ShotaroIchikawa@users.noreply.github.com>
2019-06-19 01:22:41 +0900

    Add CloudVisionAPI
    CameraExample.csを変更
    CloudVisionのLabelDetectionは動いているっぽい
    TextDetectionがうまくいかなかった

--- Assets/Scripts/CameraExample.cs
+++ Assets/Scripts/CameraExample.cs
@@ -1,4 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class CameraExample : MonoBehaviour
@@ -11,13 +17,13 @@ public class CameraExample : MonoBehaviour
             if (NativeCamera.IsCameraBusy())
                 return;
 
-            if (Input.mousePosition.x < Screen.width / 2)
+            if (Input.mousePosition.x < Screen.width / 2 && Input.mousePosition.y >Screen.width/2)
             {
                 // Take a picture with the camera
                 // If the captured image's width and/or height is greater than 512px, down-scale it
                 TakePicture(512);
             }
-            else
+            else if (Input.mousePosition.x > Screen.width / 2 && Input.mousePosition.y > Screen.width / 2)
             {
                 // Record a video with the camera
                 RecordVideo();
@@ -40,6 +46,16 @@ public class CameraExample : MonoBehaviour
                     return;
                 }
 
+                //////////////////Add for Cloud Vision API////////////////////
+                Texture2D visionTexture = Texture2DFromFile(path);
+                visionTexture.ReadPixels(new Rect(0, 0, maxSize, maxSize), 0, 0);
+                byte[] jpg = visionTexture.EncodeToJPG();
+                string encode = Convert.ToBase64String(jpg);
+
+                StartCoroutine(requestVisionAPI(encode));
+
+                //////////////////////////////////////////////////////////////////
+
                 // Assign texture to a temporary quad and destroy it after 5 seconds
                 GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
                 quad.AddComponent<MeshCollider>();
@@ -59,6 +75,7 @@ public class CameraExample : MonoBehaviour
                 // it will only be freed after a scene change
                 Destroy(texture, 5f);
             }
+            
         }, maxSize);
 
         Debug.Log("Permission result: " + permission);
@@ -78,4 +95,214 @@ public class CameraExample : MonoBehaviour
 
         Debug.Log("Permission result: " + permission);
     }
+
+    //////////////////Add for Cloud Vision API////////////////////
+    public Texture2D Texture2DFromFile(string path)
+    {
+        Texture2D texture = null;
+        if (File.Exists(path))
+        {
+            //byte取得
+            FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            BinaryReader bin = new BinaryReader(fileStream);
+            byte[] readBinary = bin.ReadBytes((int)bin.BaseStream.Length);
+            bin.Close();
+            fileStream.Dispose();
+            fileStream = null;
+            if (readBinary != null)
+            {
+                //横サイズ
+                int pos = 16;
+                int width = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    width = width * 256 + readBinary[pos++];
+                }
+                //縦サイズ
+                int height = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    height = height * 256 + readBinary[pos++];
+                }
+                //byteからTexture2D作成
+
+                texture = new Texture2D(512, 512);
+                texture.LoadImage(readBinary);
+            }
+            readBinary = null;
+        }
+        return texture;
+    }
+
+    private IEnumerator requestVisionAPI(string base64Image)
+    {
+        Debug.Log("VisionAPICalled");
+        string apiKey = "AIzaSyC_HYRYsT_fw1KO8VeSR7xJz_UzPP5rCYc";
+        string url = "https://vision.googleapis.com/v1/images:annotate?key=" + apiKey;
+
+        // requestBodyを作成
+        var requests = new requestBody();
+        requests.requests = new List<AnnotateImageRequest>();
+
+        var request = new AnnotateImageRequest();
+        request.image = new Image();
+        request.image.content = base64Image;
+
+        request.features = new List<Feature>();
+        var feature = new Feature();
+        feature.type = FeatureType.LABEL_DETECTION.ToString();
+        feature.maxResults = 10;
+        request.features.Add(feature);
+
+
+
+        requests.requests.Add(request);
+
+        // JSONに変換
+        string jsonRequestBody = JsonUtility.ToJson(requests);
+
+        // ヘッダを"application/json"にして投げる
+        var webRequest = new UnityWebRequest(url, "POST");
+        byte[] postData = Encoding.UTF8.GetBytes(jsonRequestBody);
+        webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(postData);
+        webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+
+        yield return webRequest.Send();
+
+        if (webRequest.isNetworkError)
+        {
+            // エラー時の処理
+            Debug.Log(webRequest.error);
+        }
+        else
+        {
+            // 成功時の処理
+            Debug.Log(webRequest.downloadHandler.text);
+            var responses = JsonUtility.FromJson<responseBody>(webRequest.downloadHandler.text);
+
+        }
+    }
+
+    [Serializable]
+    public class requestBody
+    {
+        public List<AnnotateImageRequest> requests;
+    }
+
+    [Serializable]
+    public class AnnotateImageRequest
+    {
+        public Image image;
+        public List<Feature> features;
+        //public string imageContext;
+    }
+
+    [Serializable]
+    public class Image
+    {
+        public string content;
+        //public ImageSource source;
+    }
+
+    [Serializable]
+    public class ImageSource
+    {
+        public string gcsImageUri;
+    }
+
+    [Serializable]
+    public class Feature
+    {
+        public string type;
+        public int maxResults;
+    }
+
+    public enum FeatureType
+    {
+        TYPE_UNSPECIFIED,
+        FACE_DETECTION,
+        LANDMARK_DETECTION,
+        LOGO_DETECTION,
+        LABEL_DETECTION,
+        TEXT_DETECTION,
+        SAFE_SEARCH_DETECTION,
+        IMAGE_PROPERTIES
+    }
+
+    [Serializable]
+    public class ImageContext
+    {
+        public LatLongRect latLongRect;
+        public string languageHints;
+    }
+
+    [Serializable]
+    public class LatLongRect
+    {
+        public LatLng minLatLng;
+        public LatLng maxLatLng;
+    }
+
+    [Serializable]
+    public class LatLng
+    {
+        public float latitude;
+        public float longitude;
+    }
+
+    [Serializable]
+    public class responseBody
+    {
+        public List<AnnotateImageResponse> responses;
+    }
+
+    [Serializable]
+    public class AnnotateImageResponse
+    {
+        public List<EntityAnnotation> labelAnnotations;
+    }
+
+    [Serializable]
+    public class EntityAnnotation
+    {
+        public string mid;
+        public string locale;
+        public string description;
+        public float score;
+        public float confidence;
+        public float topicality;
+        public BoundingPoly boundingPoly;
+        public List<LocationInfo> locations;
+        public List<Property> properties;
+    }
+
+    [Serializable]
+    public class BoundingPoly
+    {
+        public List<Vertex> vertices;
+    }
+
+    [Serializable]
+    public class Vertex
+    {
+        public float x;
+        public float y;
+    }
+
+    [Serializable]
+    public class LocationInfo
+    {
+        LatLng latLng;
+    }
+
+    [Serializable]
+    public class Property
+    {
+        string name;
+        string value;
+    }
+
+
+
 }
